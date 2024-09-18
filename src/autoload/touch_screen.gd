ba42634f27c4ce7849f99bbf3d63b1464f985849
Harmony Honey Monroe <HarmonyMonroe97@gmail.com>
2024-09-18 01:08:32 -0400

    virtual d-pad (=

--- src/autoload/touch_screen.gd
+++ src/autoload/touch_screen.gd
@@ -7,6 +7,8 @@ onready var keys := [$Control/HBoxRight/C, $Control/HBoxRight/X]
 onready var buttons := [$Control/HBoxRight/C/Control/Button, $Control/HBoxRight/X/Control/Button]
 onready var joystick := $Control/Joystick
 
+onready var btns := $Control/Joystick/Buttons.get_children()
+
 func _ready():
 	connect("visibility_changed", self, "vis")
 	
@@ -17,8 +19,6 @@ func _ready():
 func vis():
 	if is_instance_valid(UI.keys_node):
 		UI.keys_node.visible = !visible
-	if is_instance_valid(joystick):
-		joystick.is_input = visible
 
 func show_keys(arg_arrows := true, arg_c := true, arg_x := true, arg_pause := false, arg_passby := false):
 	left.visible = arg_arrows
@@ -28,9 +28,13 @@ func show_keys(arg_arrows := true, arg_c := true, arg_x := true, arg_pause := fa
 
 func set_game(arg := false):
 	var i = "" if arg else "ui_"
-	joystick.set_actions(i + "up", i + "down", i + "left", i + "right")
+	set_actions(i + "up", i + "down", i + "left", i + "right")
 	buttons[0].action = "action" if arg else "ui_no"
 	buttons[1].action = "jump" if arg else "ui_yes"
 	for f in buttons:
 		f.passby_press = arg
-	
+
+func set_actions(_up, _down, _left, _right):
+	for i in 4:
+		btns[i].action = [_right, _down, _left, _up][i]
+		btns[i].passby_press = !("ui_" in _up)
--- src/menu/round_button.gd
+++ src/menu/round_button.gd
@@ -6,6 +6,11 @@ export var points := 5 setget set_points
 export var angle := 0.0 setget set_angle
 export var deadzone := 3.0 setget set_deadzone
 
+export var poly_path : NodePath = ""
+onready var poly : Polygon2D = get_node_or_null(poly_path)
+export var inner_radius := 50.0 setget set_inner_radius
+export var inner_offset := 5.0 setget set_inner_offset
+
 func set_radius(arg := radius):
 	radius = arg
 	act()
@@ -22,17 +27,32 @@ func set_deadzone(arg := deadzone):
 	deadzone = arg
 	act()
 
+func set_inner_radius(arg := inner_radius):
+	inner_radius = arg
+	inner_act()
+
+func set_inner_offset(arg := inner_offset):
+	inner_offset = arg
+	inner_act()
+
 func act():
-	var r = Vector2(radius, 0)
+	shape = ConvexPolygonShape2D.new()
+	shape.points = make_shape()
+
+func inner_act():
+	if is_instance_valid(poly):
+		poly.polygon = make_shape(inner_radius)
+		poly.position = Vector2(inner_offset, 0).rotated(deg2rad(angle))
+
+func make_shape(_radius := radius, _points := points, _angle := angle, _deadzone := deadzone):
+	var r = Vector2(_radius, 0)
 	var vec = PoolVector2Array()
 	
 	for i in [1, 0, -1]:
-		vec.append(Vector2(deadzone, 0).rotated(deg2rad(angle + (i * 45))))
+		vec.append(Vector2(_deadzone, 0).rotated(deg2rad(_angle + (i * 45))))
 	
-	for i in points:
-		var f = i / float(points - 1)
-		vec.append(r.rotated(deg2rad(angle + lerp(-45, 45, f))))
+	for i in _points:
+		var f = i / float(_points - 1)
+		vec.append(r.rotated(deg2rad(_angle + lerp(-45, 45, f))))
 	
-	shape = ConvexPolygonShape2D.new()
-	shape.points = vec
-	
+	return vec
